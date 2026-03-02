354881828d8dc615e5eb46b1dec97ac77b778d02
Wea-sudo <916179694@qq.com>
2026-03-02 20:35:39 +0800

    update:更新后端代码

---
+++ src/main/java/com/weasudo/JavaWebApplication.java
@@ -0,0 +1,13 @@
+package com.weasudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebApplication.class, args);
+	}
+
+}
